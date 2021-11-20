27de82a0bf159b4b6dddfdfea7267bcad89922ba
rafaelsilva81 <rafaelgaldinosilva81@gmail.com>
2021-11-20 11:01:56 -0300

    Melhoria na legibilidade da saida da função main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import lorem
 import requests
 
+
 def kmp_search(pattern: str, text: str):
     """[summary]
     Busca o padrão dentro do texto de entrada.
@@ -52,6 +53,7 @@ def compute_lsp(pattern: str, m: int, lps: list):
                 lps[i] = 0
                 i += 1
 
+
 def naive_search(pattern: str, text: str):
     """[summary]
     Busca padrão dentro do texto de entrada através de duas estruturas de repetição aninhadas.
@@ -61,62 +63,66 @@ def naive_search(pattern: str, text: str):
     """
     M = len(pattern)
     N = len(text)
- 
+
     # Primeiro loop no pattern
     for i in range(N - M + 1):
         j = 0
-         
+
         # Para o atual index i, checar se o pattern está presente no texto
         while(j < M):
             if (text[i + j] != pattern[j]):
                 break
             j += 1
-        
+
         if (j == M):
             pass
             #print("Pattern found at index ", i)
+
 
 def get_texto():
     """[sumario]
     Obtém um texto através de um request para uma API geradora de texto Lorem Ipsum
     """
     req_url = 'https://asdfast.beobit.net/api/'
-    params = {'type':'paragraph','length':'2000','startLorem':'true'}
+    params = {'type': 'paragraph', 'length': '2000', 'startLorem': 'true'}
     response = requests.get(url=req_url, params=params)
     data = response.json()
 
-    return (data["text"].replace('\n',''))
+    return (data["text"].replace('\n', ''))
 
-def main(): 
-    
-    
-    #GERADOR DE LOREM IPSUM ALEATÓRIO
-    text = lorem.get_sentence(count=1, comma=(0, 2), word_range=(500000, 500000)) 
+
+def main():
+
+    word_number = 20000
+
+    # GERADOR DE LOREM IPSUM ALEATÓRIO
+    text = lorem.get_sentence(count=1, comma=(
+        0, 2), word_range=(word_number, word_number))
     #text = lorem.get_paragraph(count=2000, comma=(0, 2), word_range=(4, 10), sentence_range=(5, 10), sep='\n')
-   
-    #text = get_texto()
-    #print("\n===========================TEXTO===========================",text,"\n")
 
-    #GERADOR DE PADRÃO ALEATÓRIO
+    # GERADOR DE PADRÃO ALEATÓRIO
     pattern = lorem.get_word(count=1)
     #pattern = "consectetur"
-    print("\n========= PATTERN ESCOLHIDO : ",pattern)
-    
-    #INICIO DO TESTE KMP
-    print('\n============= KMP ================')
-    start = time.time_ns()
-    kmp_search(pattern, text)
-    end = time.time_ns()
-    t = end - start
-    print(f"tempo: {t}ns")
+    print("-----------------------------------------\n",
+          " BUSCANDO PELA PALAVRA :", pattern, "\n",
+          " TEXTO DE", word_number, "PALAVRAS\n"
+          "-----------------------------------------",
+          )
 
-    #INICIO DO TESTE NAIVE
-    print('\n============= NAIVE ================')
-    start2 = time.time_ns()
+    start = time.time()
+    kmp_search(pattern, text)
+    end = time.time()
+    t = end - start
+
+    start2 = time.time()
     naive_search(pattern, text)
-    end2 = time.time_ns()
-    t = end2 - start2
-    print(f"tempo: {t}ns")
+    end2 = time.time()
+    t2 = end2 - start2
+
+    print("| KMP   : ", f"{t:.8f} sec |", int((t * 1000000000)), "ns |")
+    print("| NAIVE : ", f"{t2:.8f} sec |", int((t2 * 1000000000)), "ns |")
+    print("-----------------------------------------")
+
 
 if __name__ == "__main__":
     main()
